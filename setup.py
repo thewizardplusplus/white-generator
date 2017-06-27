33128eb9884fab81ce39527dd3b7e9935cb829cb
thewizardplusplus <thewizardplusplus@yandex.ru>
2017-06-27 22:11:28 +0300

    Issue #19: automatically set a version number in the setup.py file

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 import setuptools
+import re
 
 packages = setuptools.find_packages()
 package_name = packages[0]
 project_name = package_name.replace('_', '-')
+with open('white_generator/consts.py') as consts_file:
+    version = re.search(
+        "^APP_VERSION = '([^']+)'$",
+        consts_file.read(),
+        re.MULTILINE,
+    ).group(1)
 setuptools.setup(
     name=project_name,
-    version='1.2',
+    version=version,
     license='MIT',
     author='thewizardplusplus',
     author_email='thewizardplusplus@yandex.ru',
-    url='https://github.com/thewizardplusplus/'+project_name,
+    url='https://github.com/thewizardplusplus/' + project_name,
     packages=packages,
     install_requires=['Pillow'],
     entry_points={
