c8d765725abe750e1a94cc5f6ebe6c1aa0735727
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-05-25 15:14:38 +0500

    Issue #26: Ignore the prepending spaces in lines in the `read_notes()` function

--- white_generator/io.py
+++ white_generator/io.py
@@ -6,17 +6,17 @@ def read_notes(notes_filename):
     with open(notes_filename) as notes_file:
         note = ''
         for line in notes_file:
-            stripped_line = line.rstrip()
+            stripped_line = line.strip()
             if stripped_line != '':
                 note += stripped_line + '\n'
                 continue
 
-            stripped_note = note.rstrip()
+            stripped_note = note.strip()
             if stripped_note != '':
                 yield stripped_note
                 note = ''
 
-        stripped_note = note.rstrip()
+        stripped_note = note.strip()
         if stripped_note != '':
             yield stripped_note
 
