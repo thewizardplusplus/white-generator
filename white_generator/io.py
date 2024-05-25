9758fd9f1437441d2e2e132ac69c4dbfda088931
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-05-25 14:59:04 +0500

    Issue #26: Add the tests for the `read_notes()` function

--- white_generator/io.py
+++ white_generator/io.py
@@ -6,14 +6,19 @@ def read_notes(notes_filename):
     with open(notes_filename) as notes_file:
         note = ''
         for line in notes_file:
-            if len(line.rstrip()) != 0:
-                note += line.rstrip() + '\n'
-            elif len(note.rstrip()) != 0:
-                yield note.rstrip()
+            stripped_line = line.rstrip()
+            if stripped_line != '':
+                note += stripped_line + '\n'
+                continue
+
+            stripped_note = note.rstrip()
+            if stripped_note != '':
+                yield stripped_note
                 note = ''
 
-        if len(note.rstrip()) != 0:
-            yield note.rstrip()
+        stripped_note = note.rstrip()
+        if stripped_note != '':
+            yield stripped_note
 
 def generate_note_id(note):
     return str(uuid.uuid5(_UUID_NAMESPACE, note))
