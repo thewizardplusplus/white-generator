e0f7479348cfecd095bf00d214b5c5661311e3c0
thewizardplusplus <thewizardplusplus@yandex.ru>
2024-06-05 01:57:17 +0500

    Issue #27: Describe the types in the `logger` module

--- white_generator/logger.py
+++ white_generator/logger.py
@@ -1,30 +1,32 @@
 import logging
 
 import termcolor
+import termcolor._types
 
-class Formatter(logging.Formatter):
-    _LEVELS_COLORS = {
-        '[INFO]': 'green',
-        '[WARNING]': 'yellow',
-        '[ERROR]': 'red',
-    }
+_LOG_FORMAT = (
+    termcolor.colored('%(asctime)s', 'grey')
+    + ' [%(levelname)s] %(message)s'
+)
+_LOG_LEVEL_COLORS: dict[str, termcolor._types.Color] = {
+    '[INFO]': 'green',
+    '[WARNING]': 'yellow',
+    '[ERROR]': 'red',
+}
 
-    def format(self, record):
+class _Formatter(logging.Formatter):
+    def format(self, record: logging.LogRecord) -> str:
         message = super().format(record)
-        for level, color in self._LEVELS_COLORS.items():
+        for level, color in _LOG_LEVEL_COLORS.items():
             message = message.replace(level, termcolor.colored(level, color))
 
         return message
 
-def get_logger():
+def get_logger() -> logging.Logger:
     return logging.getLogger(__package__)
 
-def init_logger():
+def init_logger() -> None:
     handler = logging.StreamHandler()
-    handler.setFormatter(Formatter(
-        fmt=termcolor.colored('%(asctime)s', 'grey') \
-            + ' [%(levelname)s] %(message)s',
-    ))
+    handler.setFormatter(_Formatter(_LOG_FORMAT))
 
     get_logger().addHandler(handler)
     get_logger().setLevel(logging.INFO)
